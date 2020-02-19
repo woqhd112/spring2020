7327ef065fbd6dcf59cd61f7583339115fdbb0ed
woqhd112 <woqhd1212@naver.com>
2020-02-19 17:54:01 +0900

    2 commit crud

--- sts06/src/main/java/com/bit/project01/emp/EmpController.java
+++ sts06/src/main/java/com/bit/project01/emp/EmpController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/emp/")
@@ -17,11 +18,15 @@ public class EmpController {
 		this.emp01Dao = emp01Dao;
 	}
 	
+	
+	
 	@RequestMapping("list")
 	public void list(Model model) {
 		
 		model.addAttribute("list", emp01Dao.selectAll());
 	}
+	
+	
 	
 	@RequestMapping(value="add",method=RequestMethod.GET)
 	public void add() {}
@@ -32,4 +37,48 @@ public class EmpController {
 		
 		return "redirect:list";
 	}
+	
+	
+	
+	@RequestMapping("detail")
+	public void detail(int sabun, Model model) {
+		
+		model.addAttribute("bean", emp01Dao.selectOne(sabun));
+		
+	}
+	
+	
+	
+	@RequestMapping(value="edit",method=RequestMethod.GET)
+	public void edit(@RequestParam("idx") int sabun, Model model) {
+		
+		model.addAttribute("bean", emp01Dao.selectOne(sabun));
+	}
+	@RequestMapping(value="edit",method=RequestMethod.POST)
+	public String edit(int sabun, String name, int pay) {
+		
+		emp01Dao.updateOne(sabun, name, pay);
+		
+		return "redirect:detail?sabun="+sabun;
+	}
+	
+	
+	
+	@RequestMapping("delete")
+	public String delete(@RequestParam("idx") int sabun) {
+		
+		int result=emp01Dao.deleteOne(sabun);
+		return "redirect:list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
