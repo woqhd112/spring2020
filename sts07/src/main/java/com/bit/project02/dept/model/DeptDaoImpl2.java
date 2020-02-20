2bda9f9ba8f1eeec53a6ed0781dadcb35378a6ff
woqhd112 <woqhd1212@naver.com>
2020-02-20 19:50:31 +0900

    11 commit mybatis

--- sts07/src/main/java/com/bit/project02/dept/model/DeptDaoImpl2.java
+++ sts07/src/main/java/com/bit/project02/dept/model/DeptDaoImpl2.java
@@ -4,38 +4,37 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.ibatis.SqlMapClientTemplate;
 
 import com.bit.project02.dept.model.entity.DeptVo;
-import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class DeptDaoImpl2 implements DeptDao {
 	
-	@Autowired
-	SqlMapClientTemplate sqlMapClientTemplate;
+//	@Autowired
+//	SqlMapClientTemplate sqlMapClientTemplate;
 	
 	@Override
 	public List<DeptVo> selectAll() {
 		
-		return sqlMapClientTemplate.queryForList("dept.selectAll");
+//		return sqlMapClientTemplate.queryForList("dept.selectAll");
+		return null;
 	}
 
 	@Override
 	public void insertOne(String dname, String loc) {
 		
-		sqlMapClientTemplate.insert("dept.insertOne",new DeptVo(0,dname,loc));
+//		sqlMapClientTemplate.insert("dept.insertOne",new DeptVo(0,dname,loc));
 	}
 
 	@Override
 	public DeptVo selectOne(int key) {
-		
-		return (DeptVo) sqlMapClientTemplate.queryForObject("dept.selectOne",key);
+		return null;
+//		return (DeptVo) sqlMapClientTemplate.queryForObject("dept.selectOne",key);
 	}
 
 	@Override
 	public int updateOne(int deptno, String dname, String loc) {
-		
-		return sqlMapClientTemplate.update("dept.updateOne",new DeptVo(deptno,dname,loc));
+		return 0;
+//		return sqlMapClientTemplate.update("dept.updateOne",new DeptVo(deptno,dname,loc));
 	}
 
 }
