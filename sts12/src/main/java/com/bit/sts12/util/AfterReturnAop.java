2a2dc6a5168b30db8db5495be94d0fbc09259211
woqhd112 <woqhd1212@naver.com>
2020-02-25 15:52:52 +0900

    aop commit

--- sts12/src/main/java/com/bit/sts12/util/AfterReturnAop.java
+++ sts12/src/main/java/com/bit/sts12/util/AfterReturnAop.java
@@ -4,13 +4,13 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class AfterReturnAop {
 
 	//리턴값이있는..
 	//해당 메소드의 리턴값을 가져올수있음
-	@AfterReturning(pointcut = "execution(* com.bit.sts12.service.EmpService.updateOne(..))",returning = "result")
+//	@AfterReturning(pointcut = "execution(* com.bit.sts12.service.EmpService.updateOne(..))",returning = "result")
 	public void after(Object result) {
 		System.out.println("결과로 "+result+"를 리턴");
 	}
--- sts12/src/main/java/com/bit/sts12/util/BeforeAop.java
+++ sts12/src/main/java/com/bit/sts12/util/BeforeAop.java
@@ -3,15 +3,12 @@ package com.bit.sts12.util;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class BeforeAop {
 
-	@Before("execution(* com.bit.sts12.service.EmpServiceImpl.select*(..))")
+//	@Before("execution(* com.bit.sts12.service.EmpServiceImpl.select*(..))")
 	public void before(JoinPoint join) {
 		System.out.println(join);
 		System.out.println("Aop before시점에 출력");
