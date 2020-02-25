9bb13bfcab455f0cf2b4920d56aa58e392561cb1
woqhd112 <woqhd1212@naver.com>
2020-02-25 16:30:42 +0900

    1

--- sts12/src/main/java/com/bit/sts12/service/EmpServiceImpl.java
+++ sts12/src/main/java/com/bit/sts12/service/EmpServiceImpl.java
@@ -1,10 +1,14 @@
 package com.bit.sts12.service;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
+import com.bit.sts12.model.EmpDao;
 import com.bit.sts12.model.entity.EmpVo;
 
 @Service
@@ -12,37 +16,39 @@ public class EmpServiceImpl implements EmpService {
 
 	Logger log=LoggerFactory.getLogger(getClass());
 	
+	@Autowired
+	SqlSession sqlSession;
+	
+	
 	@Override
 	public void selectAll(Model model) {
 
-		System.out.println("리스트 출력...");
+		model.addAttribute("list", sqlSession.getMapper(EmpDao.class).selectAll());
 	}
 
 	@Override
 	public void selectOne(int key, Model model) {
-
-		System.out.println("레코드 출력...");
+		
+		model.addAttribute("bean", sqlSession.getMapper(EmpDao.class).selectOne(key));
 	}
 
+	@Transactional//메소드 전체를 트랜잭션처리해줌
 	@Override
 	public void insertOne(EmpVo bean) throws Exception {
 		
-		System.out.println("데이터베이스에 입력...");
-		int su=0/0;
+		sqlSession.getMapper(EmpDao.class).insertOne(bean);
 	}
 
 	@Override
 	public int updateOne(EmpVo bean) {
 		
-		System.out.println("레코드 수정...");
-		return 1;
+		return sqlSession.getMapper(EmpDao.class).updateOne(bean);
 	}
 
 	@Override
 	public int deleteOne(int key) {
 		
-		System.out.println("레코드 삭제...");
-		return 0;
+		return sqlSession.getMapper(EmpDao.class).deleteOne(key);
 	}
 
 }
--- sts12/src/test/java/com/bit/sts12/service/EmpServiceTest.java
+++ sts12/src/test/java/com/bit/sts12/service/EmpServiceTest.java
@@ -1,12 +1,18 @@
 package com.bit.sts12.service;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ui.Model;
+
+import com.bit.sts12.model.entity.EmpVo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/applicationContext.xml")
@@ -17,15 +23,59 @@ public class EmpServiceTest {
 	
 	@Test
 	public void test() {
-		service.selectOne(0,null);
-		service.selectAll(null);
-		try {
-			service.insertOne(null);
-		} catch (Exception e) {
+		
+		Model model=null;
+		model=new Model() {
+			Map<String, Object> map=new HashMap<String, Object>();
+			@Override
+			public Model mergeAttributes(Map<String, ?> attributes) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public boolean containsAttribute(String attributeName) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Map<String, Object> asMap() {
+				// TODO Auto-generated method stub
+				return map;
+			}
+			
+			@Override
+			public Model addAttribute(String attributeName, Object attributeValue) {
+				
+				map.put(attributeName, attributeValue);
+				return this;
+			}
+			
+			@Override
+			public Model addAttribute(Object attributeValue) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Model addAllAttributes(Map<String, ?> attributes) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Model addAllAttributes(Collection<?> attributeValues) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+		service.selectAll(model);
+		Map<String, Object> map = model.asMap();
+		List<EmpVo> list=(List<EmpVo>) map.get("list");
+		for(EmpVo bean:list) {
+			System.out.println(bean);
 		}
-		service.updateOne(null);
-		service.deleteOne(0);
-		assertNotNull(service);
 	}
 
 }
