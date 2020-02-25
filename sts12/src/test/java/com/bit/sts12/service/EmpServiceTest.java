5eb0b518cedb552ddf97c70bdcf853ab7288f024
woqhd112 <woqhd1212@naver.com>
2020-02-25 16:31:33 +0900

    2

--- sts12/src/test/java/com/bit/sts12/service/EmpServiceTest.java
+++ sts12/src/test/java/com/bit/sts12/service/EmpServiceTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.Model;
